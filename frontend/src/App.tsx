5b28c6350be0f702630e7bc2778fe17bc62a0a42
thangnguyenvn98 <thangnguyenvn647@gmail.com>
2024-03-29 14:06:14 -0400

    Added about page and some components from shadcn as well as static files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,44 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 import './global.css'
+import AboutUs from "./components/about-us";
 
 
 function App() {
   
 
   return (
-    <>
-   <div className="container p-4">
-    <h2 className="text-green-500 text-center mb-4">Hello</h2>
-    <h3 className="text-yellow-400 text-center">Yello Text</h3>
-   </div>
-      
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            
+              <div>
+                Hello There testing
+              </div>
+            
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            
+              <AboutUs />
+            
+          }
+        />
+       
+
+       
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   )
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import "./global.css";
+import { ThemeProvider } from './components/theme-provider.tsx';
+import {QueryClientProvider, QueryClient} from "@tanstack/react-query"
 
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+     <App />
+    </ThemeProvider>
+    </QueryClientProvider>
+   
   </React.StrictMode>,
 )
