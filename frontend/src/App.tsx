d5711d625fae6bd12c4509792db69852cc5c2b48
QuanMichaelNguyen <144957908+QuanMichaelNguyen@users.noreply.github.com>
2024-04-01 11:20:49 -0400

    Second commit login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,29 @@
 import { TabsDemo } from "./components/ui/tabdemo";
 import "./global.css";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import AboutUs from "./components/about-us";
 
 function App() {
-  return <TabsDemo />;
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<div className="bg-white ">Hello There testing</div>}
+        />
+        <Route path="/login" element={<TabsDemo />} />
+
+        <Route path="/about" element={<AboutUs />} />
+
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 import "./global.css";
-import { ThemeProvider } from './components/theme-provider.tsx';
-import {QueryClientProvider, QueryClient} from "@tanstack/react-query"
+import { ThemeProvider } from "./components/theme-provider.tsx";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-     <App />
-    </ThemeProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+      </ThemeProvider>
     </QueryClientProvider>
-   
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
