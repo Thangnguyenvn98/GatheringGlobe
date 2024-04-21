4560ac62c6123e56269e4c9a71e4674fdd3ef050
Phuong-Le-NN <le_p2@denison.edu>
2024-04-21 14:27:29 -0400

    reformat bg image banner

--- frontend/src/components/homepage/events.tsx
+++ frontend/src/components/homepage/events.tsx
@@ -1,14 +1,10 @@
 import EventBanner from "../../images/diversity_inclusion1.jpg"
-// import {DatePickerDemo} from "./searchdate"
-// import { Form } from "./formfooter";
-//import * as React from "react"
 
 
 function Events (){
     return (
         <div className="flex flex-col w-full overflow-hidden">
-            <div style={{backgroundImage: `url("${EventBanner}")` }} className="bg-opacity-20 h-[400px]" >
-            </div>
+            <div style={{backgroundImage: `url("${EventBanner}")`, backgroundSize:'cover', backgroundRepeat:'no-repeat'}} className="flex flex-col h-[400px]"> </div>
         </div>
     )
 }
