f4c33d46de31dbe3da448c7fd1ff9f5b4f613ec5
quynhtran158 <quynhtran.3002@gmail.com>
2024-05-04 23:30:04 -0400

    add onclick to whole event card (text and img)

--- frontend/src/components/Discover_Event/shared/EventCard.tsx
+++ frontend/src/components/Discover_Event/shared/EventCard.tsx
@@ -13,14 +13,20 @@ const EventCard: React.FC<Omit<Event, "id">> = ({
   onClick,
 }) => {
   return (
-    <div className="event-card-container flex mb-5 bg-white shadow-md rounded-lg p-4">
+    <div
+      className="event-card-container flex mb-5 bg-white shadow-md rounded-lg p-4"
+      onClick={onClick}
+    >
       <img
         src={imageUrls[0]} // Assuming the first URL is the primary image
         alt={title}
         className="w-60 h-auto object-contain rounded-lg mr-4 "
       />
 
-      <div className="flex-1 flex flex-col justify-center pl-4">
+      <div
+        className="flex-1 flex flex-col justify-center pl-4"
+        onClick={onClick}
+      >
         <div className="event-description">
           <h1 className="text-lg font-bold py-2">{title}</h1>
           <h2 className="text-sm text-gray-600 py-2">{artistName}</h2>
@@ -30,13 +36,6 @@ const EventCard: React.FC<Omit<Event, "id">> = ({
             <h3 className="py-2">End: {endTime}</h3>
             <h3>Location: {location}</h3>
           </div>
-          <button
-            className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-auto"
-            onClick={onClick}
-          >
-            View Event Details
-            {/* dunfg link react */}
-          </button>
         </div>
       </div>
     </div>
