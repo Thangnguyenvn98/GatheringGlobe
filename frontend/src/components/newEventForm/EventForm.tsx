0897f2ff92f87422e6492b82680807a7c46ee8f3
thangnguyenvn98 <thangnguyenvn647@gmail.com>
2024-06-10 16:50:08 -0400

    Merged pr with updated changes to backend for event creation 2

--- frontend/src/components/newEventForm/EventForm.tsx
+++ frontend/src/components/newEventForm/EventForm.tsx
@@ -186,6 +186,7 @@ const EventForm = () => {
       form.reset();
       toast.success(response.data.message);
       const event = response.data;
+      navigate(`/`);
       console.log(event);
     } catch (error) {
       toast.error("Something went wrong");
