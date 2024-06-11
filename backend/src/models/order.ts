b805b8f4e5ff1263a272ef9bff430ffff01ce45a
thangnguyenvn98 <thangnguyenvn647@gmail.com>
2024-06-11 13:28:30 -0400

    Send pdf to email success, need configring for each tickets QR Code

--- backend/src/models/order.ts
+++ backend/src/models/order.ts
@@ -15,31 +15,53 @@ export type OrderType = {
   firstName: string;
   lastName: string;
   email: string;
-  paymentMethodId:string;
+  paymentMethodId: string;
   paymentIntentId: string;
 };
 
-const orderSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  events: [
-    {
-      eventId: { type: mongoose.Schema.Types.ObjectId, ref: "Event", required: true },
-      tickets: [
-        {
-          ticketId: { type: mongoose.Schema.Types.ObjectId, ref: "Ticket", required: true },
-          quantity: { type: Number, required: true },
-        },
-      ],
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-  ],
-  totalPrice: { type: Number, required: true },
-  paymentStatus: { type: String, required: true, enum: ['pending', 'completed', 'failed'], default: 'pending' },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true },
-  paymentMethodId: { type: String, required: true },
-  paymentIntentId: { type: String, required: true },
-});
+    events: [
+      {
+        eventId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Event",
+          required: true,
+        },
+        tickets: [
+          {
+            ticketId: {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "Ticket",
+              required: true,
+            },
+            quantity: { type: Number, required: true },
+          },
+        ],
+      },
+    ],
+    totalPrice: { type: Number, required: true },
+    paymentStatus: {
+      type: String,
+      required: true,
+      enum: ["pending", "completed", "failed"],
+      default: "pending",
+    },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    paymentMethodId: { type: String, required: true },
+    paymentIntentId: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Order = mongoose.model<OrderType>("Order", orderSchema);
 export default Order;
